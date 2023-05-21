65283b52761571e34cb47a838c9e58902459287a
상현메인 <handsone1213@gmail.com>
2023-05-21 16:44:46 +0900

    no message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 10071;
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
-var originWhitelist = ['59.10.79.188'];
+originWhitelist.push('59.10.79.188');
 function parseEnvList(env) {
   if (!env) {
     return [];
